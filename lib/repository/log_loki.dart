30f63d88e307c3a83c5b972e304b28a5f6d4a34f
AnaCarolinaMoreiraMtBank <ana.moreira@mtbank.com.br>
2024-12-05 16:04:45 -0300

    removed unused import

--- lib/repository/log_loki.dart
+++ lib/repository/log_loki.dart
@@ -1,9 +1,6 @@
 import 'package:logger/logger.dart';
 import 'package:logger_app/external/loki_server.dart';
 
-import '../domain/logger_usecase_imp.dart';
-import '../external/loki_server_imp.dart';
-
 class LogLoki extends LogOutput{
   final LokiServer lokiServer;
 
